bed4167f086a3cfeeb5e6208a329ddaed4777865
Javier sanchez <javier.ant.sanchezc@gmail.com>
2020-11-22 09:03:30 -0500

    updatye server .js
    dpppp

--- src/server.js
+++ src/server.js
@@ -8,4 +8,6 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
+
+
